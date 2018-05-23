f8f53624385247d61a04f8926aa87c1ed399db78
Igor Miheev <igor7mih@gmal.com>
2018-05-23 03:46:03 +0300

    add show and hide progress bar

--- app/src/main/java/labs/igor/labs/MainActivity.java
+++ app/src/main/java/labs/igor/labs/MainActivity.java
@@ -2,12 +2,31 @@ package labs.igor.labs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        progressBar = findViewById(R.id.progress_bar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(progressBar.getVisibility() == View.GONE){
+                    progressBar.setVisibility(View.VISIBLE);
+                    button.setText(R.string.hide_progress_bar);
+                }else {
+                    progressBar.setVisibility(View.GONE);
+                    button.setText(R.string.show_progress_bar);
+                }
+            }
+        });
     }
 }
